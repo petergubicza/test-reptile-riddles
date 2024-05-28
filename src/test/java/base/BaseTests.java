9ecded1b2c0773cc8cd14c4b730f4386a1bd788b
Gergo Fazekas <144519526+gergofazekas92@users.noreply.github.com>
2024-05-28 10:33:32 +0200

    add enviroment variable for driver location

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,12 +1,10 @@
 package base;
 
-import com.codecool.reptile.pages.LoginPage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,7 +13,6 @@ import java.time.Duration;
 public class BaseTests {
     WebDriver driver;
     private By usernameField = By.id("user-name");
-    //private By usernameField = By.xpath("//input[@type = 'text']");
     private By passwordField = By.id("password");
     private By loginButton = By.xpath("//button[text() = 'LOGIN']");
     private String username = "test";
@@ -24,7 +21,7 @@ public class BaseTests {
 
     @BeforeEach
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "D:\\Codecool\\projects\\Advanced\\test-reptile-riddles\\src\\test\\resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", System.getenv("DRIVER_LOCATION"));
         driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get("http://localhost:3000/login");
